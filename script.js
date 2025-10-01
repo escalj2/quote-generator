cd8d334216d37ca487f4dfece1893ddcf2e0004e
escalj2 <escalj2@gmail.com>
2025-09-30 19:18:15 -0700

    simplified api fetch request and made it so it only displays necessary JSON data

--- script.js
+++ script.js
@@ -1,49 +1,19 @@
-const button = document.getElementById("clickme");
+var quotetext = "";
+const button = document.getElementById('clickme');
+document.getElementById("quote-text").innerHTML = quotetext;
 
 //Define API URL
 const apiURL ='https://api.kanye.rest/';
 
-
-// Make a GET request
-fetch(apiURL)
-    .then(Response => {
-        // Check if network response was a success
-        if (!Response.ok){
-            throw new Error('HTTP error! status: ${response.status}');
-        }
-        // Parse the JSON data from the response body
-        return Response.json();
-    })
-    .then(data => {
-        //Handle the retrieved data here
-        console.log('API data', data);
-        //Call a function to display the data in HTML
-        displayDataInHtml(data);
-    })
-    .catch(error => {
-        //Handle any errors that occurred during the fetch operation
-        console.error('Error fetching data:', error)
-    });
-
-function displayDataInHtml(data) {
-    //make div with id of data-container
-    const dataContainer = document.getElementById('data-container');
-
-    //clear previous content if any
-    dataContainer.innerHTML = '';
-
-    const paragraph = document.createElement('p');
-    paragraph.textContent = `Data: ${JSON.stringify(data)}`;
-    dataContainer.appendChild(paragraph);
-}
-
-
-
-
-document.getElementById("quote").innerHTML = clicks;
-
 function handleClick(){
-    document.getElementById("quote").innerHTML = clicks;
-    displayDataInHtml(data);
+
+    fetch(apiURL)
+        .then(response => response.json()) //parse JSON from response
+        .then(data => {
+            document.getElementById("quote-text").innerHTML = data.quote;
+        })
+        .catch(error => {
+            console.error("Error: ", error)
+    });
 }
-button.addEventListener("click", handleClick);
+button.addEventListener("click", handleClick)
